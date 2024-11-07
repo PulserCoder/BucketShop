ccbf70ed71f2ab40446ba0cdec2eb41c62efda10
PulserCoder <92484659+PulserCoder@users.noreply.github.com>
2024-11-07 17:32:46 +0300

    Initial commit

---
+++ src/main/java/pro/sky/BucketShop/BucketShopApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.BucketShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BucketShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BucketShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/BucketShop/BucketShopApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.BucketShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BucketShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
